3dbccd6a5d98adb341ad2561d3dffc5fe1e0002e
Jordan Liss <jordanliss13@gmail.com>
2023-11-07 15:37:20 -0800

    Remove unnecessary zoox question scripts

--- Zoox_assignment_q_1/zoox_assignment_q_1.cpp
+++ Zoox_assignment_q_1/zoox_assignment_q_1.cpp
@@ -12,7 +12,7 @@ using namespace std;
 // Function for taking a string and creating a "checksum" in ASCII
 // Reminder: The input string should be more than just the "command"
 //           Review problem description for more info
-unsigned char checksum(string input) {
+unsigned char checksum(std::string input) {
   unsigned char accumulated = 0;
   for(char& c : input) {
     accumulated += c;
@@ -21,23 +21,23 @@ unsigned char checksum(string input) {
 }
 
 // Required output format
-const string MSG_HEADER = "ZOOX";
+const std::string MSG_HEADER = "ZOOX";
 
-const string MSG_LEFT = "LEFT";
-const string MSG_RIGHT = "RIGHT";
-const string MSG_FORWARD = "FORWARD";
-const string MSG_BEEP = "BEEP";
+const std::string MSG_LEFT = "LEFT";
+const std::string MSG_RIGHT = "RIGHT";
+const std::string MSG_FORWARD = "FORWARD";
+const std::string MSG_BEEP = "BEEP";
 
-const string OUTPUT_LEFT = "L";
-const string OUTPUT_RIGHT = "R";
-const string OUTPUT_FORWARD = "F";
-const string OUTPUT_BEEP = "!";
+const std::string OUTPUT_LEFT = "L";
+const std::string OUTPUT_RIGHT = "R";
+const std::string OUTPUT_FORWARD = "F";
+const std::string OUTPUT_BEEP = "!";
 
 // You should not need to call this function
 // Util used by execute_packet_command(...)
-void execute_command(string cmd, int repeat) {
+void execute_command(std::string cmd, int repeat) {
   for (int i = 0; i < repeat; i++) {
-    cout << cmd;
+    std::cout << cmd;
   }
 }
 
@@ -45,7 +45,7 @@ void execute_command(string cmd, int repeat) {
 // Provide the content of the command packet as inputs.
 // cmd should be the command string from the packet
 // repeat should be the repeat byte from the packet
-void execute_packet_command(string cmd, int repeat) {
+void execute_packet_command(std::string cmd, int repeat) {
   if (cmd.compare(MSG_LEFT) == 0) {
     execute_command(OUTPUT_LEFT, repeat);
   }
@@ -75,7 +75,7 @@ ParseState msgState = ParseState::HEADER;
 // Required prototype and main
 void parse(char c);
 int main() {
-  string input;
+  std::string input;
 //   input = "ZOOX6RIGHT3X";
   input = "ZOOX5LEFT2QZOOX5BEEP3C";
 //   cin >> input;
@@ -114,7 +114,7 @@ void parse(char c)
 {
 
     // Concat the char input
-    string byteChar(1,c);
+    std::string byteChar(1,c);
     currentPacket=currentPacket + byteChar;
     // currentPacket.append(byteChar);
 
@@ -222,17 +222,17 @@ void parse(char c)
                 std::string checkString(1,checkChar);
                 if (checkString == currentPacket)
                 {
-                // Collect packets into one command
-                execute_packet_command(commandString, stoi(repeatPacket));
-                
-                currentPacket ="";
-                // Reset parser state machine
-                msgState=ParseState::HEADER;
+                    // Collect packets into one command
+                    execute_packet_command(commandString, stoi(repeatPacket));
+                    
+                    currentPacket ="";
+                    // Reset parser state machine
+                    msgState=ParseState::HEADER;
                 }
                 else
                 {
-                // Reset parser state machine
-                msgState=ParseState::HEADER; 
+                    // Reset parser state machine
+                    msgState=ParseState::HEADER; 
                 }
             }
         break;
