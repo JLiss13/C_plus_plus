4d0931ceae870980998e3932f0478b588784bf3b
Jordan Liss <jordanliss13@gmail.com>
2023-11-08 21:05:44 -0800

    Added lookup and remove capabilities

--- ZTM_bst/ztm_bst.cpp
+++ ZTM_bst/ztm_bst.cpp
@@ -5,7 +5,7 @@
 //
 
 #include <iostream>
-
+#include <cmath>
 class Node {
 public:
     int data;
@@ -63,7 +63,94 @@ public:
     }
     int Lookup(int value)
     {
-        return -999;
+        Node* newNode = new Node(value);
+        parent = root;
+        int output = -999;
+        while(parent->right || parent->left)
+        {
+            if (value > parent->data)
+            {
+                if (parent->right->data == value)
+                {
+                    output = parent->right->data;
+                    std::cout << "Find the value:" << output<<std::endl;
+                    break;
+                }
+                else
+                {
+                    current=parent->right;
+                }            
+            }
+            else
+            {
+                if (parent->left->data == value)
+                {
+                    output = parent->left->data;
+                    std::cout << "Find the value: " << output<<std::endl;
+                    break;
+                }
+                else
+                {
+                    current=parent->left;
+                }
+            }
+            parent = current;
+        }
+        if (output==-999)
+        {
+            std::cout << "Could not find value:" << value <<std::endl;
+        }
+        return output;
+    }
+    void Remove(int value)
+    {
+        // To hold onto the parent node logic the next highest value (meaning the value to the right) gets put in place of the removed node
+
+        // Have three nodes to keep track of: prevNode, newReplacementNode and take the next highest value and replace the removed node with the next highest value node. Move associated pointers along with it
+        Node* removedNode = new Node();
+        Node* prevNode = new Node();
+        Node* newNode = new Node();
+        Node* newReplacementNode = new Node();
+        parent = root;
+        int output = -999;
+
+        // First find the node to be removed
+        while(!removedNode->right && !removedNode->left)
+        {
+            if (value > parent->data)
+            {
+                current=parent->right;        
+            }
+            else if (value < parent->data)
+            {
+                current=parent->left;
+            }
+            else if ((value == parent->data))
+            {
+                removedNode = parent;
+            }
+            parent = current;
+        }
+
+        // Setup the replacement node
+        int prevDist = std::abs(current->data - removedNode->data);
+        while(!parent->right)
+        {
+            current=parent->right;
+            int newDist = std::abs(current->data - removedNode->data);
+            // Find the closest to the value in the right side of tree to the parent
+            if (newDist < prevDist) 
+            {
+                newReplacementNode = current;
+            }
+            parent = current;
+        }
+
+        // Now reassign the pointers [TODO]
+        // 3 Scenarios
+        // 1) parent->right == nullptr
+        // 2) parent->right
+        // 3) parent->right == nullptr && parent->left
     }
     void DisplayParent()
     {
@@ -124,5 +211,7 @@ int main()
     tree.Insert(2);
     tree.Insert(30);
     tree.Print();
+    tree.Lookup(5);
+    tree.Lookup(1222);
     return 0;
 }
